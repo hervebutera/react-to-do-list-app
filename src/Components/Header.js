5c0adbd44a32197a9d0130abdb274cc541ae04c7
--replace-all <h.buterak@gmail.com>
2023-02-15 09:33:30 +0200

    fix: fix theme changing not applying on first rendering

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -3,40 +3,42 @@ import { ReactComponent as SunIcon } from "../Assets/images/icon-sun.svg";
 import { useState } from "react";
 
 const Header = () => {
-  const [themeIcon, setThemeIcon] = useState("moon");
-
   const body = document.body;
   const darkTheme = "dark";
   const lightTheme = "light";
   let theme;
 
-  if (localStorage) {
+  if (localStorage["theme"]) {
     theme = localStorage.getItem("theme");
+  } else {
+    theme = lightTheme;
   }
 
-  if (theme === lightTheme || theme === darkTheme) {
-    body.classList.add(theme);
+  const [themeIcon, setThemeIcon] = useState(theme);
+
+  if (theme === darkTheme) {
+    body.classList.add(darkTheme);
   } else {
     body.classList.add(lightTheme);
   }
 
   const themeToggleHandler = () => {
-    if (!theme || theme === lightTheme) {
+    if (theme === lightTheme) {
       body.classList.replace(lightTheme, darkTheme);
-      localStorage.setItem("theme", "dark");
-      setThemeIcon("sun");
+      localStorage.setItem("theme", darkTheme);
+      setThemeIcon(darkTheme);
     } else {
       body.classList.replace(darkTheme, lightTheme);
-      localStorage.setItem("theme", "light");
-      setThemeIcon("moon");
+      localStorage.setItem("theme", lightTheme);
+      setThemeIcon(lightTheme);
     }
   };
 
   return (
     <div className="header">
       <h1 className="ToDo-head">T O D O</h1>
-      <span className="theme-icon" onClick={() => themeToggleHandler()}>
-        {themeIcon === "moon" ? <MoonIcon /> : <SunIcon />}
+      <span className="theme-icon" onClick={themeToggleHandler}>
+        {themeIcon === "light" ? <MoonIcon /> : <SunIcon />}
       </span>
     </div>
   );
