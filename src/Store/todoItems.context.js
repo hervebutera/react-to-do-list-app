ee5aaba85ec2a0eb8443e8f815a1b5b9e5313368
--replace-all <h.buterak@gmail.com>
2023-02-21 21:59:58 +0200

    feat: add removing a todo and updating completion status

--- src/Store/todoItems.context.js
+++ src/Store/todoItems.context.js
@@ -4,18 +4,37 @@ export const ToDoItemsContext = createContext({
   toDos: [],
   totalToDos: 0,
   addToDo: (toDoItemInfo) => {},
-  //   removeToDoItem: (toDoItemId) => {},
-  //   isCompleted: (toDoItemId) => {},
+  removeToDo: (toDoItemId) => {},
+  isCompletedStatusToggle: (toDoItemId) => {},
 });
 
 export const ToDoItemsContextProvider = (props) => {
   const [toDoItems, setToDoItems] = useState(() => {
     return JSON.parse(localStorage.getItem("toDoItems")) || [];
   });
-
+   //localStorage.removeItem("toDoItems");
   const addToDoItemHandler = (toDoItemInfo) => {
     setToDoItems((currentToDoItems) => {
       return currentToDoItems.concat(toDoItemInfo);
+    });
+  };
+
+  const removeToDoItemHandler = (toDoItemId) => {
+    setToDoItems((currentToDoItems) => {
+      return currentToDoItems.filter((todo) => {
+        return todo.id !== toDoItemId;
+      });
+    });
+  };
+
+  const isCompletedStatusToggleHandler = (toDoItemId) => {
+    setToDoItems((currentToDoItems) => {
+      return currentToDoItems.map((todoItem) => {
+        if (toDoItemId === todoItem.id) {
+          todoItem.isCompleted = !todoItem.isCompleted;
+        }
+        return todoItem;
+      });
     });
   };
 
@@ -29,6 +48,8 @@ export const ToDoItemsContextProvider = (props) => {
     toDos: toDoItems,
     totalToDos: toDoItems.length,
     addToDo: addToDoItemHandler,
+    removeToDo: removeToDoItemHandler,
+    isCompletedStatusToggle: isCompletedStatusToggleHandler,
   };
 
   return (
