f21f1fb76a1a44a59fdb59ec405cad49231748e0
--replace-all <h.buterak@gmail.com>
2023-02-22 12:58:20 +0200

    feat: add left todos display and clearing completed todos

--- src/Components/BottomNavBar.js
+++ src/Components/BottomNavBar.js
@@ -1,12 +1,40 @@
+import { useContext, useEffect, useState } from "react";
+import { ToDoItemsContext } from "../Store/todoItems.context";
+
 const BottomNavBar = () => {
+  const ToDoItemsCtx = useContext(ToDoItemsContext);
+  const [unCompletedToDosLength, setUnCompletedToDosLength] = useState(() => {
+    const unCompletedToDos = ToDoItemsCtx.toDos.filter((toDo) => {
+      return toDo.isCompleted === false;
+    });
+    return unCompletedToDos.length;
+  });
+
+  useEffect(() => {
+    setUnCompletedToDosLength(() => {
+      const unCompletedToDos = ToDoItemsCtx.toDos.filter((toDo) => {
+        return toDo.isCompleted === false;
+      });
+      return unCompletedToDos.length;
+    });
+  }, [ToDoItemsCtx.toDos]);
+
+
   return (
     <div className="bottomNav">
-      <span className="bottom-li items-count">5 items left</span>
+      <span className="bottom-li items-count">
+        {`${unCompletedToDosLength} items left`}
+      </span>
       <ul>
         <li className="bottom-li nav-li">All</li>
         <li className="bottom-li nav-li">Active</li>
         <li className="bottom-li nav-li">Completed</li>
-        <li className="bottom-li clear-btn">Clear completed</li>
+        <li
+          className="bottom-li clear-btn"
+          onClick={() => ToDoItemsCtx.clearCompletedToDos()}
+        >
+          Clear completed
+        </li>
       </ul>
     </div>
   );
